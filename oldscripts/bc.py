7ced6ef96d1ad0c136d1db02403fce7b480c9563
Maurice Mengel <mauricemengel@gmail.com>
2019-09-07 11:22:05 +0200

    cleanup files

--- oldscripts/bc.py
+++
@@ -1,9 +0,0 @@
-import treepoem
-
-data='OB001891312'
-for each in ['azteccode', 'pdf417', 'code128', 'code39']:
-    image = treepoem.generate_barcode(
-        barcode_type=each,  # One of the BWIPP supported codes.
-        data=data,
-    )
-    image.convert('1').save(each+data+'.png')
