b14176b5e81e74df76ca8d41e8b005074c503e9f
Maurice Mengel <mauricemengel@gmail.com>
2019-09-07 11:28:51 +0200

    correct typos

--- oldscripts/bc3.py
+++
@@ -1,9 +0,0 @@
-from pubcode import Code128
-barcode= Code128('OB00189137', charset='A')
-barcode.image(height=40, module_width=1).save('pubcode-A.png')
-
-barcode= Code128('OB00189137', charset='B')
-barcode.image(height=40, module_width=1).save('pubcode-B.png')
-
-barcode= Code128('OB00189137', charset='A')
-barcode.image(height=40, module_width=1).save('pubcode-C.png')
